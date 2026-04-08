b17743731b37916204fcae18a34cb725d8beed68
BetheeLiz <elizabethimaobongthomas@gmail.com>
2026-04-09 00:14:18 +0100

    Delete month-one-assessment directory

--- month-one-assessment/user_data/db_server_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install postgresql14 -y
-yum install postgresql-server -y
-postgresql-setup initdb
-systemctl start postgresql
-systemctl enable postgresql
--- month-one-assessment/user_data/web_server_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Welcome to TechCorp - Instance ID: $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" > /var/www/html/index.html
